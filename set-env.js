3ca34f6ab5518bf676ea7e0c3b51fe5d81ad53c7
Pietro Antonio Nini <pietroantonio99@icloud.com>
2025-07-21 19:36:17 +0200

    Aggiornamento dell'URL dell'API nel file di configurazione ambientale per puntare al nuovo backend di agriturismo.

--- set-env.js
+++ set-env.js
@@ -4,7 +4,7 @@ const targetPath = './src/environments/environment.prod.ts';
 
 const envConfigFile = `export const environment = {
   production: true,
-  apiUrl: '${process.env.API_URL || 'https://api.production.com'}',
+  apiUrl: '${process.env.API_URL || 'https://flat-damselfly-agriturismo-backend-47075869.koyeb.app/'}',
   appName: '${process.env.APP_NAME || 'Agriturismo Management'}',
   supabaseUrl: '${process.env.SUPABASE_URL || ''}',
   supabaseAnonKey: '${process.env.SUPABASE_ANON_KEY || ''}'
